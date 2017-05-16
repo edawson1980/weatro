eaf18ba87b344caa41b4c3482890d567678f3a33
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-05-16 19:43:13 -0400

    CSS Overhall

--- angular/weatherAjax.js
+++ angular/weatherAjax.js
@@ -1,8 +1,7 @@
 $("#showButton").on("click", () => {
-  // Make sure to add your API key to the URL!
   var city = $("#city").html()
   var state = $("#state").html()
-  var url = 'https://api.wunderground.com/api/6e29343528bfa584/geolookup/conditions/q/' + state + '/' + city + '.json'
+  var url = 'https://api.wunderground.com/api/928e6919af2dbe8a/geolookup/conditions/q/' + state + '/' + city + '.json'
   $.ajax({
     url: url,
     type: "get",
